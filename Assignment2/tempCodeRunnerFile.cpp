794beed8851fd81a936efe48d770bb42080e6284
EmmanuelMarianMat <emmanuelmarianmat@gmail.com>
2021-07-02 11:55:29 +0530

    temp deleted

--- Assignment2/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-tree.insert(5);
-    tree.insert(2);
-    tree.insert(1);
-    tree.insert(3);
-    tree.insert(10);
-    tree.insert(7);
-    tree.insert(15);
-    tree.insert(11);
-    tree.insert(20);
